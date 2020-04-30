a43926570c34cd1857c5c0770fa9f362c902fa51
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-30 15:02:31 -0400

    modified reservation function to use SQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -41,8 +41,8 @@ const getUserWithId = function(id) {
   return pool
   .query(
     `
-SELECT * FROM users
-WHERE id = $1;
+  SELECT * FROM users
+  WHERE id = $1;
 `,
     [id]
   )
@@ -79,7 +79,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(
+    `SELECT reservations.*, properties.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    JOIN property_reviews ON property_reviews.property_id = reservations.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id, properties.title, cost_per_night
+    ORDER BY start_date DESC
+    LIMIT $2;`,
+    [guest_id, limit]
+  )
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
